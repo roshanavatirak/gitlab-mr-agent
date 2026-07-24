38974563db2919c333acc7f0c62e82023342af33
Roshan Avatirak <144925122+roshanavatirak@users.noreply.github.com>
2026-07-24 16:47:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import List, Optional
 from fastapi import FastAPI, Header, HTTPException, Request, status
 from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
